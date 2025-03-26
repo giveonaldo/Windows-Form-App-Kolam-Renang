f6bd6a9e8e110a4d48b3c8305ed2847758e8096c
giveonaldo <giveonaldo@gmail.com>
2025-03-26 13:50:34 +0700

    Add project files.

---
+++ coba-1/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace coba_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblHeading = new System.Windows.Forms.Label();
+            this.lblInstruction = new System.Windows.Forms.Label();
+            this.lblNama = new System.Windows.Forms.Label();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.lblNo = new System.Windows.Forms.Label();
+            this.txtNo = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblHeading
+            // 
+            this.lblHeading.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblHeading.AutoSize = true;
+            this.lblHeading.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold);
+            this.lblHeading.Location = new System.Drawing.Point(182, 40);
+            this.lblHeading.Name = "lblHeading";
+            this.lblHeading.Size = new System.Drawing.Size(449, 22);
+            this.lblHeading.TabIndex = 0;
+            this.lblHeading.Text = "Selamat Datang di Kolam Renang Pacific Ocean";
+            // 
+            // lblInstruction
+            // 
+            this.lblInstruction.AutoSize = true;
+            this.lblInstruction.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstruction.Location = new System.Drawing.Point(60, 96);
+            this.lblInstruction.Name = "lblInstruction";
+            this.lblInstruction.Size = new System.Drawing.Size(401, 18);
+            this.lblInstruction.TabIndex = 1;
+            this.lblInstruction.Text = "Isi data diri berikut, untuk dapat melakukan pemesanan tiket.";
+            // 
+            // lblNama
+            // 
+            this.lblNama.AutoSize = true;
+            this.lblNama.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNama.Location = new System.Drawing.Point(60, 137);
+            this.lblNama.Name = "lblNama";
+            this.lblNama.Size = new System.Drawing.Size(48, 18);
+            this.lblNama.TabIndex = 2;
+            this.lblNama.Text = "Nama";
+            // 
+            // txtNama
+            // 
+            this.txtNama.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNama.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNama.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtNama.Location = new System.Drawing.Point(63, 162);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(300, 26);
+            this.txtNama.TabIndex = 3;
+            // 
+            // lblNo
+            // 
+            this.lblNo.AutoSize = true;
+            this.lblNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNo.Location = new System.Drawing.Point(60, 210);
+            this.lblNo.Name = "lblNo";
+            this.lblNo.Size = new System.Drawing.Size(99, 18);
+            this.lblNo.TabIndex = 4;
+            this.lblNo.Text = "No Whatsapp";
+            // 
+            // txtNo
+            // 
+            this.txtNo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNo.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtNo.Location = new System.Drawing.Point(63, 235);
+            this.txtNo.Name = "txtNo";
+            this.txtNo.Size = new System.Drawing.Size(300, 26);
+            this.txtNo.TabIndex = 5;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.Color.Lime;
+            this.btnSubmit.Font = new System.Drawing.Font("Futura Bk", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSubmit.Location = new System.Drawing.Point(63, 304);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(142, 51);
+            this.btnSubmit.TabIndex = 6;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(455, 152);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(315, 254);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtNo);
+            this.Controls.Add(this.lblNo);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.lblNama);
+            this.Controls.Add(this.lblInstruction);
+            this.Controls.Add(this.lblHeading);
+            this.Name = "Form1";
+            this.Text = "Kolam Renang Pacific Ocean";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHeading;
+        private System.Windows.Forms.Label lblInstruction;
+        private System.Windows.Forms.Label lblNama;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.Label lblNo;
+        private System.Windows.Forms.TextBox txtNo;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ coba-1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace coba_1
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            TextBox txtNama = new TextBox();
+            TextBox txtEmail = new TextBox();
+
+            if (txtNama.Text != "" || txtEmail.Text != "")
+            {
+                MessageBox.Show("Data tidak boleh kosong", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                MessageBox.Show("Data sudah diisi", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                MessageBox.Show($"Data berhasil disimpan!\nNama : {txtNama.Text}\nEmail : {txtEmail.Text}", "Sukses!!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                txtEmail.Clear();
+                txtNama.Clear();
+            }
+        }
+
+        
+    }
+}
